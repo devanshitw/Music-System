1f4526205266bc730f0552668eb206baac66b911
devanshitw <56011944+devanshitw@users.noreply.github.com>
2024-05-06 01:55:26 +0530

    rt-ert

--- api/cors.js
+++ api/cors.js
@@ -3,6 +3,7 @@ const allowCors = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  console.log('res: ', res);
   next();
 };
 
